742b99ac4e046b26a24cc183cdd21329d8093d76
abir <abir.sakif@gmail.com>
2018-07-02 18:26:15 -0700

    test cURL call successful in datatable

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use GuzzleHttp\Client;
 
 class SalesOrderController extends Controller
 {
@@ -13,7 +15,26 @@ class SalesOrderController extends Controller
      */
     public function index()
     {
-        return view('sales');
+        $client = new Client;
+
+        $response = $client->post('http://54.68.18.149:9696/public/order-list', [
+            'headers' => [
+                'APPAUTHID' => 'intel_pharma',
+                'APPTOKEN' => '3fb8744b8ef6f65ed9d5687c4f45e6dd',
+            ],
+            'form_params' => [
+                'company_id' => 7,
+            ],
+        ]);
+
+        // You need to parse the response body
+        // This will parse it into an array
+        $response = json_decode($response->getBody(), true);
+        $order_list = $response['data']['order_list'];
+        //print_r($order_list);
+
+        $name = 'San Juan Vacation';
+        return view('sales', ['orders' => $order_list]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/sales', 'SalesOrderController@index')->name('sales');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
